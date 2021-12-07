359c037ba94fe406b496b5bc16ca2de06a94c36e
Bryan Burgers <bryan@burgers.io>
2021-12-07 07:14:06 -0600

    07b

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -6,6 +6,8 @@ fn main() -> Result<()> {
     let crabs = parse(include_str!("input.txt"))?;
     let result_a = solve_a(&crabs);
     println!("{}", result_a);
+    let result_b = solve_b(&crabs);
+    println!("{}", result_b);
 
     Ok(())
 }
@@ -44,6 +46,33 @@ fn solve_a(crabs: &[Crab]) -> i64 {
     least_fuel_used
 }
 
+fn solve_b(crabs: &[Crab]) -> i64 {
+    let mut min_position = i16::MAX;
+    let mut max_position = i16::MIN;
+    for crab in crabs {
+        if crab.horizontal_position < min_position {
+            min_position = crab.horizontal_position;
+        }
+        if crab.horizontal_position > max_position {
+            max_position = crab.horizontal_position;
+        }
+    }
+
+    let mut least_fuel_used = i64::MAX;
+    for target_position in min_position..=max_position {
+        let mut fuel_used = 0;
+
+        for crab in crabs {
+            fuel_used += crab.fuel_used_for_position_2(target_position);
+        }
+        if fuel_used < least_fuel_used {
+            least_fuel_used = fuel_used;
+        }
+    }
+
+    least_fuel_used
+}
+
 #[derive(Debug, Copy, Clone)]
 struct Crab {
     horizontal_position: i16,
@@ -52,6 +81,11 @@ struct Crab {
 impl Crab {
     fn fuel_used_for_position(&self, x: i16) -> i64 {
         (self.horizontal_position - x).abs().into()
+    }
+
+    fn fuel_used_for_position_2(&self, x: i16) -> i64 {
+        let difference: i64 = (self.horizontal_position - x).abs().into();
+        (difference * (difference + 1)) / 2
     }
 }
 
@@ -87,6 +121,32 @@ mod tests {
         let crabs = parse(include_str!("example.txt"))?;
         let result = solve_a(&crabs);
         assert_eq!(result, 37);
+
+        Ok(())
+    }
+
+    #[test]
+    fn fuel_used_2() -> Result<()> {
+        let crab = Crab {
+            horizontal_position: 16,
+        };
+        assert_eq!(crab.fuel_used_for_position_2(16), 0);
+        assert_eq!(crab.fuel_used_for_position_2(17), 1);
+        assert_eq!(crab.fuel_used_for_position_2(18), 1 + 2);
+        assert_eq!(crab.fuel_used_for_position_2(19), 1 + 2 + 3);
+        assert_eq!(crab.fuel_used_for_position_2(20), 1 + 2 + 3 + 4);
+        assert_eq!(crab.fuel_used_for_position_2(15), 1);
+        assert_eq!(crab.fuel_used_for_position_2(14), 1 + 2);
+        assert_eq!(crab.fuel_used_for_position_2(13), 1 + 2 + 3);
+        assert_eq!(crab.fuel_used_for_position_2(12), 1 + 2 + 3 + 4);
+        Ok(())
+    }
+
+    #[test]
+    fn test_2() -> Result<()> {
+        let crabs = parse(include_str!("example.txt"))?;
+        let result = solve_b(&crabs);
+        assert_eq!(result, 168);
 
         Ok(())
     }
