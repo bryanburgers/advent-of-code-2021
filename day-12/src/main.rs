3573373e5e6bf1ddccbd8aec60ec4636fd5294aa
Bryan Burgers <bryan@burgers.io>
2021-12-13 08:44:01 -0600

    12b

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,12 +1,17 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     fmt::Debug,
+    hash::Hash,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = include_str!("input.txt");
     let graph = Graph::parse(input)?;
     let paths = graph.paths()?;
+    let result = paths.count();
+    println!("{}", result);
+
+    let paths = graph.paths_part_b()?;
     let result = paths.count();
     println!("{}", result);
     Ok(())
@@ -79,6 +84,10 @@ impl<'s> Graph<'s> {
     pub fn paths<'g>(&'g self) -> Result<PathsIterator<'g, 's>, &'static str> {
         PathsIterator::new(self)
     }
+
+    pub fn paths_part_b<'g>(&'g self) -> Result<PathsIteratorPartB<'g, 's>, &'static str> {
+        PathsIteratorPartB::new(self)
+    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -131,6 +140,12 @@ impl<'g, 's> Eq for Vertex<'g, 's> {}
 impl<'g, 's> Debug for Vertex<'g, 's> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.name())
+    }
+}
+
+impl<'g, 's> Hash for Vertex<'g, 's> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
     }
 }
 
@@ -206,6 +221,92 @@ impl<'g, 's> PathsIterator<'g, 's> {
 }
 
 impl<'g, 's> Iterator for PathsIterator<'g, 's> {
+    type Item = Vec<Vertex<'g, 's>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_path()
+    }
+}
+
+pub struct PathsIteratorPartB<'g, 's> {
+    stack_vertices: Vec<Vertex<'g, 's>>,
+    stack_iterators: VecDeque<EdgesIterator<'g, 's>>,
+}
+
+impl<'g, 's> PathsIteratorPartB<'g, 's> {
+    pub fn new(graph: &'g Graph<'s>) -> Result<Self, &'static str> {
+        let start = graph
+            .vertex_by_name("start")
+            .ok_or("start vertex not found")?;
+        let edges = start.edges();
+        let stack_vertices = vec![start];
+        let mut stack_iterators = VecDeque::new();
+        stack_iterators.push_front(edges);
+        Ok(Self {
+            stack_iterators,
+            stack_vertices,
+        })
+    }
+
+    fn would_be_valid_path(&self, vertex: &Vertex<'_, '_>) -> bool {
+        if vertex.is_start() {
+            // Can't revisit start twice.
+            return false;
+        }
+        if vertex.is_big() {
+            // We can always go to a big cave.
+            return true;
+        }
+
+        let mut seen_twice = false;
+        let mut seen_vertexes = HashSet::new();
+
+        for vertex in self.stack_vertices.iter().chain(std::iter::once(vertex)) {
+            if vertex.is_big() {
+                continue;
+            }
+            let already_seen = !seen_vertexes.insert(vertex);
+            if already_seen {
+                if seen_twice {
+                    // Another one has already been seen twice!
+                    return false;
+                } else {
+                    seen_twice = true;
+                }
+            }
+        }
+
+        true
+    }
+
+    pub fn next_path(&mut self) -> Option<Vec<Vertex<'g, 's>>> {
+        loop {
+            let iterator = self.stack_iterators.back_mut()?;
+            if let Some(next_vertex) = iterator.next() {
+                if next_vertex.is_end() {
+                    // Found an end. Return the entire path!
+                    let mut ret = self.stack_vertices.clone();
+                    ret.push(next_vertex);
+                    return Some(ret);
+                } else if !self.would_be_valid_path(&next_vertex) {
+                    // Would not be a valid path. Ignore
+                } else {
+                    // Found the next cave. Add it to the stack and "recurse"
+                    let edges = next_vertex.edges();
+                    self.stack_vertices.push(next_vertex);
+                    self.stack_iterators.push_back(edges);
+                }
+            } else {
+                // We've looked at all of the current cave's edges. Pop it off and continue with the
+                // previous cave.
+                self.stack_vertices.pop();
+                self.stack_iterators.pop_back();
+            }
+        }
+    }
+}
+
+impl<'g, 's> Iterator for PathsIteratorPartB<'g, 's> {
     type Item = Vec<Vertex<'g, 's>>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -386,5 +487,79 @@ mod tests {
         let paths = graph.paths().expect("Expected paths");
         let count = paths.take(1000 /* to catch infinite recursion */).count();
         assert_eq!(count, 226);
+    }
+
+    #[test]
+    fn test_paths_b_on_example_1() {
+        let graph = Graph::parse(
+            r#"
+                start-A
+                start-b
+                A-c
+                A-b
+                b-d
+                A-end
+                b-end
+            "#,
+        )
+        .expect("Expected valid parse");
+
+        let paths = graph.paths_part_b().expect("Expected paths");
+        let count = paths.take(100 /* to catch infinite recursion */).count();
+        assert_eq!(count, 36);
+    }
+
+    #[test]
+    fn test_paths_b_on_example_2() {
+        let graph = Graph::parse(
+            r#"
+                dc-end
+                HN-start
+                start-kj
+                dc-start
+                dc-HN
+                LN-dc
+                HN-end
+                kj-sa
+                kj-HN
+                kj-dc
+            "#,
+        )
+        .expect("Expected valid parse");
+
+        let paths = graph.paths_part_b().expect("Expected paths");
+        let count = paths.take(1000 /* to catch infinite recursion */).count();
+        assert_eq!(count, 103);
+    }
+
+    #[test]
+    fn test_paths_b_on_example_3() {
+        let graph = Graph::parse(
+            r#"
+                fs-end
+                he-DX
+                fs-he
+                start-DX
+                pj-DX
+                end-zg
+                zg-sl
+                zg-pj
+                pj-he
+                RW-he
+                fs-DX
+                pj-RW
+                zg-RW
+                start-pj
+                he-WI
+                zg-he
+                pj-fs
+                start-RW
+            "#,
+        )
+        .expect("Expected valid parse");
+
+        let paths = graph.paths_part_b().expect("Expected paths");
+        let count = paths.take(10000 /* to catch infinite recursion */).count();
+        assert_eq!(count, 3509);
     }
 }
